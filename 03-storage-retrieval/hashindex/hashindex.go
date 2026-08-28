00ae3a714330ec5caf727d5c30b49ce0b18df7c1
rijalghodi <rijalghodi.dev@gmail.com>
2026-08-28 19:09:48 +0700

    use uint32 as hash index

--- 03-storage-retrieval/hashindex/hashindex.go
+++ 03-storage-retrieval/hashindex/hashindex.go
@@ -1,20 +1,20 @@
 package hashindex
 
 type HashIndex struct {
-    items map[string]int32
+    items map[string]uint32
 }
 
 func NewHashIndex() *HashIndex {
 	return &HashIndex{
-		items: map[string]int32{},
+		items: map[string]uint32{},
 	}
 }
 
-func (h *HashIndex) Put(key string, offset int32) {
+func (h *HashIndex) Put(key string, offset uint32) {
 	h.items[key] = offset
 }
 
-func (h *HashIndex) Get(key string) (int32, bool) {
+func (h *HashIndex) Get(key string) (uint32, bool) {
 	value, ok := h.items[key]
 	return value, ok
 }
